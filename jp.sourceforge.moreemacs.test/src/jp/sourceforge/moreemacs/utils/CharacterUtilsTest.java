f5d1f6d5c67b15fce063e1b66ad7f18d599cbd6d
quiver2k <quiver2k@users.sourceforge.jp>
2009-08-02 02:51:11 +0900

    added default locale test

--- jp.sourceforge.moreemacs.test/src/jp/sourceforge/moreemacs/utils/CharacterUtilsTest.java
+++ jp.sourceforge.moreemacs.test/src/jp/sourceforge/moreemacs/utils/CharacterUtilsTest.java
@@ -7,9 +7,18 @@ import org.junit.Test;
 public class CharacterUtilsTest {
     @Test
     public void testGetWidth() {
-        String str = "α■";
-        for(int codePoint : CodePointIterator.each(str)) {
+        String ambiguousChars = "α■";
+
+        for(int codePoint : CodePointIterator.each(ambiguousChars)) {
             assertEquals(2, CharacterUtils.getWidth(codePoint, Locale.JAPANESE));
+        }
+        Locale.setDefault(Locale.JAPANESE);
+        for(int codePoint : CodePointIterator.each(ambiguousChars)) {
+            assertEquals(2, CharacterUtils.getWidth(codePoint));
+        }
+        Locale.setDefault(Locale.ENGLISH);
+        for(int codePoint : CodePointIterator.each(ambiguousChars)) {
+            assertEquals(1, CharacterUtils.getWidth(codePoint));
         }
     }
 }
