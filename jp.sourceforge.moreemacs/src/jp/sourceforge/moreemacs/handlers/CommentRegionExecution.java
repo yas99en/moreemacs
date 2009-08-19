0c6972942d07a0aaa0e801abb52bb65b30fe736e
quiver2k <quiver2k@users.sourceforge.jp>
2009-08-20 00:41:51 +0900

    changed comment region

--- jp.sourceforge.moreemacs/src/jp/sourceforge/moreemacs/handlers/CommentRegionExecution.java
+++ jp.sourceforge.moreemacs/src/jp/sourceforge/moreemacs/handlers/CommentRegionExecution.java
@@ -1,6 +1,7 @@
 package jp.sourceforge.moreemacs.handlers;
 
 import org.eclipse.core.commands.Command;
+import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.ui.commands.ICommandService;
 import org.eclipse.ui.handlers.IHandlerService;
 
@@ -8,21 +9,30 @@ import org.eclipse.ui.handlers.IHandlerService;
 public final class CommentRegionExecution extends TextEditorExecution {
     @Override
     public void execute() throws Exception {
+        Command command = getEnabledToggleCommentCommand();
+        if(command == null) {
+            return;
+        }
+        // if the selection is empty, the marked region will be new selection.
+        getSelection(true);
+        
+        IHandlerService handlerService =
+            (IHandlerService)textEditor.getSite().getService(IHandlerService.class);
+        handlerService.executeCommand(command.getId(), null);
+
+        ITextSelection selection = getSelection(false);
+        textViewer.setSelectedRange(selection.getOffset()+selection.getLength(), 0);
+    }
+    
+    private Command getEnabledToggleCommentCommand() {
         ICommandService commandService =
             (ICommandService)textEditor.getSite().getService(ICommandService.class);
         for(Command command: commandService.getDefinedCommands()) {
-            if(!isEnabledToggleCommentCommand(command)) {
-                continue;
+            if(isEnabledToggleCommentCommand(command)) {
+                return command;
             }
-            
-            // if the selection is empty, the marked region will be new selection.
-            getSelection(true);
-            
-            IHandlerService handlerService =
-                (IHandlerService)textEditor.getSite().getService(IHandlerService.class);
-            handlerService.executeCommand(command.getId(), null);
-            return;
         }
+        return null;
     }
     
     private boolean isEnabledToggleCommentCommand(Command command) {
