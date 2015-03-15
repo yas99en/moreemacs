a08b6d137464403939048d8819669e409ea3b730
quiver2k <quiver2k@users.sourceforge.jp>
2015-03-15 15:35:39 +0900

    Modified cleanup.sh and makearchive.sh.

--- jp.sourceforge.moreemacs.updatesite/makearchive.sh
+++ jp.sourceforge.moreemacs.updatesite/makearchive.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 if [ $#  -lt 1 ]; then
-    echo "usage: $0 2.0.0"
+    echo "usage: $0 version"
+    echo " ex) \$ $0 2.0.0"
     exit
 fi
+
+files="artifacts.jar content.jar features plugins site.xml"
+
+for file in $files; do
+    if [ ! -e $file ]; then
+        echo "$file not found"
+        exit
+    fi
+done
 
 version=$1
 name=moreemacs_$version
 mkdir $name
-cp -r artifacts.jar content.jar features plugins site.xml $name
+cp -r $files $name
 zip -r $name.zip $name
 rm -rf $name
